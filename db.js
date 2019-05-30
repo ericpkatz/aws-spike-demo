446894a4fa3ea0a74319484822d51bdcc10ce492
Eric P. Katz <ericpkatz@gmail.com>
2019-05-30 19:13:10 -0400

    hash usernames and authenticate methods

--- db.js
+++ db.js
@@ -1,11 +1,56 @@
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL);
 const ImageUploader = require('./ImageUploader');
+const bcrypt = require('bcrypt');
 
 const User = conn.define('user', {
   name: Sequelize.STRING,
-  imageURL: Sequelize.STRING
+  imageURL: Sequelize.STRING,
+  password: Sequelize.STRING
+}, {
+  defaultScope: {
+    attributes: {
+      exclude: ['password']
+    }
+  },
+  scopes: {
+    withPassword: {
+
+    }
+  },
+  hooks: {
+    beforeSave: function(user){
+      if(user.password){
+        return bcrypt.hash(user.password, 5)
+          .then( hash => user.password = hash);
+      }
+    }
+  }
 });
+
+User.authenticate = function({ name, password }){
+  let _user;
+  return this.scope('withPassword').findOne({ where: { name }})
+    .then( user => {
+      if(!user){
+        const error = new Error('bad credentials');
+        error.status = 401;
+        throw error;
+      }
+      _user = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then( authenticated => {
+      if(authenticated){
+        return _user;
+      }
+      const error = new Error('bad credentials');
+      error.status = 401;
+      throw error;
+    });
+  
+}
+
 
 User.prototype.upload = function(data){
   return ImageUploader.upload(data)
@@ -18,7 +63,12 @@ User.prototype.upload = function(data){
 
 const syncAndSeed = ()=> {
   return conn.sync({ force: true })
-    .then(()=> User.create({ name: 'moe' }));
+    .then(()=> {
+      return Promise.all([
+        User.create({ name: 'larry', password: 'foo' }),
+        User.create({ name: 'moe', password: 'bar' })
+      ]);
+    });
 };
 
 module.exports = {
--- models.spec.js
+++ models.spec.js
@@ -20,5 +20,40 @@ describe('Models', ()=> {
         .then( user => expect(user.imageURL).to.equal('foo.png'));
 
     });
+    it('password does come back with user', ()=> {
+      return User.findOne({ where: { name: 'moe' }})
+        .then( user => expect(user.password).not.to.be.ok)
+
+    });
+    it('a user can log in with correct credentials', ()=> {
+      const credentials = {
+        name: 'moe',
+        password: 'bar'
+      };
+      return User.authenticate(credentials)
+        .then( user => expect(user.name).to.equal('moe'));
+
+    });
+    it('a user can not log in with bad name', ()=> {
+      const credentials = {
+        name: 'mo',
+        password: 'bar'
+      };
+      return User.authenticate(credentials)
+        .catch( ex => expect(ex.status).to.equal(401));
+
+    });
+    it('a user can not log in with bad password', ()=> {
+      const credentials = {
+        name: 'moe',
+        password: 'ba'
+      };
+      return User.authenticate(credentials)
+        .then(()=> {
+          throw new Error('noooooo');
+        }) 
+        .catch( ex => expect(ex.status).to.equal(401));
+
+    });
   });
 });
