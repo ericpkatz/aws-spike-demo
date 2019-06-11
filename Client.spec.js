91f25f7cead2e0ecc4a7fe0f17052e6badbc688a
Eric P. Katz <ericpkatz@gmail.com>
2019-06-10 20:21:11 -0400

    client-side-cli

---
+++ Client.spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+import mapper from './client/mapper';
+import App from './client/App';
+describe('Client', ()=> {
+  it('can use import', ()=> {
+    expect(true).to.equal(true);
+  });
+});
